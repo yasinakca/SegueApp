751c5a3e91da0e9008ed6123332e5034fc0a1698
YASIN AKCA <yasin.akca@bil.omu.edu.tr>
2020-04-16 18:21:23 +0300

    segue usage in swift

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # SegueApp
+
+A swift app which is about how to use 'Segue' and how to transfer data between two or more screens.
--- SegueApp/ViewController.swift
+++ SegueApp/ViewController.swift
@@ -10,11 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var name: String?
+    
+    @IBOutlet weak var nameText: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func nextClicked(_ sender: Any) {
+        name = nameText.text!
+        performSegue(withIdentifier: "toSecondVC", sender: nil)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "toSecondVC" {
+            let destination = segue.destination as! SecondViewController
+            destination.selectedName = name
+        }
+    }
+    
 }
 
